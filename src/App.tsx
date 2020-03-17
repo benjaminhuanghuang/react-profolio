8f20334eaf5cce11863ac57a6e95b62b1c736ac3
Benjamin Huang <bhuang@rms.com>
2020-03-16 23:33:43 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
 //
-import Home from "./components/Home"
-import About from "./components/About"
+import Particles from "react-particles-js";
+//
+import Home from "./components/Home";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+//
+import "./App.css";
+
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: "absolute"
+  }
+});
 
 function App() {
+  const classes = useStyles()
+
   return (
     <Router>
       <Navbar />
+      <Header />
+      <Particles className={classes.particlesCanva}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+              density:{
+                enable: true,
+                value_area: 900
+              }
+            }
+          }
+        }}
+      ></Particles>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+//
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+//
+import Typed from "react-typed";
+//
+import avatar from "../img/avatar.png";
+
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto"
+    // width: theme.spacing(13),
+    // height: theme.spacing(13)
+  },
+  title: {
+    color: "tomato"
+  },
+  typeContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1
+  }
+}));
 
 export default function Header() {
+  const classes = useStyles();
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Box className={classes.typeContainer}>
+      <Grid container justify="center">
+        <Avatar src={avatar} alt="Ben" className={classes.avatar}></Avatar>
+      </Grid>
+
+      <Typography variant="h4" className={classes.title}>
+        <Typed
+          strings={["React", "TypeScript"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        ></Typed>
+      </Typography>
+    </Box>
+  );
 }
